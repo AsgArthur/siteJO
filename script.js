37940514440269896e09b63fefb4d6e2df23f57b
NeirdaG <gadrieng@gmail.com>
2025-01-19 14:42:10 +0100

    edit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "20%";
+    document.getElementById("overlay").style.display = "block"; // Affiche l'overlay
+}
+
+function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("overlay").style.display = "none"; // Masque l'overlay
+}
